81d258ef36d9401ca310b21ef96d311b866a9d0d
omniaura <omniaura5@gmail.com>
2013-09-11 22:37:32 -0700

    switched to parallax/cover theme, skeleton

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).stellar({
+        horizontalScrolling: false,
+        verticalOffset: 0
+    });
+    /*
+    $('.contents-overlay').blurjs({
+        source: '.section',
+        radius: 7,
+        overlay: 'rgba(255,255,255,0.4)'
+    });
+    */
+});
