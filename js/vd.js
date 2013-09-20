881a28b1360a3d5b21e85da6332101c6c6bfda1c
omniaura <omniaura5@gmail.com>
2013-09-19 19:46:03 -0700

    sp styling, ae fade-in effect, vd styling, content, and js refactoring

--- js/vd.js
+++ js/vd.js
@@ -1,20 +1,17 @@
 // Use below link if you want to get latitude & longitude
 // http://www.findlatitudeandlongitude.com/find-address-from-latitude-and-longitude.php
 
-$(document).ready(function(){
-    main_video_icon = "img/vd/map-marker-blue.png"
+var main_video_icon = "img/vd/map-marker-blue.png"
+function makeMap(markers) {
     
-    function youtubeEmbed(url) {
-        return '<iframe width="560" height="315" src="'+url+'" frameborder="0" allowfullscreen></iframe>'
-    }
     //set up markers 
-    var places = {"markers": [
-                    {"latitude": "34.050224", "longitude":"-118.245450", "icon": main_video_icon, "baloon_text": youtubeEmbed("http://www.youtube.com/embed/OolQsN5eZFI")},
-                    {"latitude": "34.047787", "longitude":"-118.249483", "icon": main_video_icon, "baloon_text": youtubeEmbed("http://www.youtube.com/embed/DvyLH7EKQd4")},
-                    {"latitude": "34.089914", "longitude":"-118.229198", "icon": main_video_icon, "baloon_text": youtubeEmbed("http://www.youtube.com/embed/9mTG72stghY")},
-                    {"latitude": "34.061921", "longitude":"-118.259036", "icon": main_video_icon, "baloon_text": youtubeEmbed("http://www.youtube.com/embed/9mTG72stghY")}
-            ]
+    var places = {"markers": []
     };
+    
+    //add the places passed as args
+    for (i = 0; i < arguments.length; i++) {
+        places['markers'].push(arguments[i]);
+    }
     
     //set up map options
     $("#map").mapmarker({
@@ -22,5 +19,8 @@ $(document).ready(function(){
         center  : 'Dodger Stadium, Los Angeles, CA',
         markers : places
     });
+}
 
-});
+function youtubeEmbed(url) {
+    return '<iframe width="560" height="315" src="'+url+'" frameborder="0" allowfullscreen></iframe>'
+}
